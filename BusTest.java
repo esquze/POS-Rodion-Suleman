30b1d937fb54a23c06ef69d8f1f1036f3ae8cd72
esquze <rodion.bal@gmail.com>
2025-11-03 15:17:22 +0100

    bluej

---
+++ BusTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class BusTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class BusTest
+{
+    /**
+     * Default constructor for test class BusTest
+     */
+    public BusTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
